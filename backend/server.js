676c4c3b0d077f472f6479fe33b2b78640389407
sunil <skpatelsk61@gmail.com>
2026-05-02 22:05:58 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ const { Server } = require("socket.io");
 const { setOffline, getOnlineUsers } = require('./src/models/visitorModel');
 const emailService = require('./src/utils/emailService');
 
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || 'localhost';
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const startServer = async () => {
   console.log('🚀 Starting server...');
